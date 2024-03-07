a332d9d67aa8f2ac14413de30445533f4a4adf93
sareenarazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-07 17:54:24 -0500

    Extract and improve turtle code
    1. Take width and heights as arguments to new
    2. Add bounds check for canvas

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,12 @@
-use image::{DynamicImage, ImageResult, Rgb, RgbImage};
-use std::path::PathBuf;
-use num_traits::float::FloatConst;
+mod turtle;
 
 fn main() {
-    let mut turtle = Turtle::new();
-    turtle.x = 0;
-    turtle.y = 0;
+    let mut turtle = turtle::Turtle::new(100, 100);
     for _ in 1..=4 {
-        turtle.forward(10);
+        turtle.forward(200);
         turtle.turn(90.0);
     }
 
     turtle.save("img.png".into()).unwrap()
 }
 
-struct Turtle {
-    buffer: RgbImage,
-    x: u32,
-    y: u32,
-    angle: f64,
-}
-
-impl Turtle {
-    fn new() -> Turtle {
-        Turtle {
-            buffer: RgbImage::new(32, 32),
-            x: 0,
-            y: 0,
-            angle: 0.0,
-        }
-    }
-
-    fn forward(&mut self, distance: u32) {
-        let new_x = (self.x as i32 + (f64::sin(self.angle) * distance as f64) as i32) as u32;
-        let new_y = (self.y as i32 + (f64::cos(self.angle) * distance as f64) as i32) as u32;
-        self.plot_line(self.x, self.y, new_x, new_y);
-        self.x = new_x;
-        self.y = new_y;
-    }
-
-    fn turn(& mut self, angle: f64) {
-        self.angle = (self.angle + (angle / 360.0 * f64::TAU())) % f64::TAU();
-    }
-
-
-    // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-    fn plot_line(&mut self, x0: u32, y0: u32, x1: u32, y1: u32) {
-        let mut x0 = x0;
-        let mut y0 = y0;
-        let dx = i32::abs(x1 as i32 - x0 as i32);
-        let sx: i32 = if x0 < x1 { 1 } else { -1 };
-
-        let dy = -i32::abs(y1 as i32 - y0 as i32);
-        let sy: i32 = if y0 < y1 { 1 } else { -1 };
-
-        let mut error = dx + dy;
-
-        loop {
-            self.buffer.put_pixel(x0, y0, Rgb([255, 0, 0]));
-            if x0 == y0 && y0 == y1 {
-                break
-            }
-            let e2 = 2 * error;
-            if e2 >= dy {
-                if x0 == x1 { break }
-                error = error + dy;
-                x0 = (x0 as i32 + sx) as u32;
-            }
-            if e2 <= dx {
-                if y0 == y1 { break }
-                error = error + dx;
-                y0 = (y0 as i32 + sy) as u32;
-            }
-        }
-    }
-
-    fn save(&self, path: PathBuf) -> ImageResult<()> {
-        DynamicImage::ImageRgb8(self.buffer.clone()).save(path)
-    }
-}
---
+++ src/turtle.rs
@@ -0,0 +1,75 @@
+use std::path::PathBuf;
+use image::{DynamicImage, ImageResult, Rgb, RgbImage};
+use num_traits::float::FloatConst;
+
+pub struct Turtle {
+    buffer: RgbImage,
+    x: i64,
+    y: i64,
+    angle: f64,
+}
+
+impl Turtle {
+    pub fn new(width: u32, height: u32) -> Turtle {
+        Turtle {
+            buffer: RgbImage::new(width, height),
+            x: 0,
+            y: 0,
+            angle: 0.0,
+        }
+    }
+
+    pub fn forward(&mut self, distance: u32) {
+        let new_x = self.x + (f64::sin(self.angle) * distance as f64)as i64;
+        let new_y = self.y + (f64::cos(self.angle) * distance as f64) as i64;
+        self.plot_line(self.x, self.y, new_x, new_y);
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    pub fn turn(& mut self, angle: f64) {
+        self.angle = (self.angle + (angle / 360.0 * f64::TAU())) % f64::TAU();
+    }
+
+
+    // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    fn plot_line(&mut self, x0: i64, y0: i64, x1: i64, y1: i64) {
+        let mut x0 = x0;
+        let mut y0 = y0;
+        let dx = i64::abs(x1 - x0);
+        let sx: i64 = if x0 < x1 { 1 } else { -1 };
+
+        let dy = -i64::abs(y1 - y0);
+        let sy: i64 = if y0 < y1 { 1 } else { -1 };
+
+        let mut error = dx + dy;
+
+        loop {
+            self.put_pixel(x0, y0);
+            if x0 == y0 && y0 == y1 {
+                break
+            }
+            let e2 = 2 * error;
+            if e2 >= dy {
+                if x0 == x1 { break }
+                error = error + dy;
+                x0 = x0  + sx;
+            }
+            if e2 <= dx {
+                if y0 == y1 { break }
+                error = error + dx;
+                y0 = y0 + sy;
+            }
+        }
+    }
+
+    fn put_pixel(&mut self, x: i64, y: i64) {
+        if x >= 0 && x < self.buffer.width() as i64 && y >= 0 && y < self.buffer.height() as i64 {
+            self.buffer.put_pixel(x as u32, y as u32, Rgb([255, 0, 0]));
+        }
+    }
+
+    pub fn save(&self, path: PathBuf) -> ImageResult<()> {
+        DynamicImage::ImageRgb8(self.buffer.clone()).save(path)
+    }
+}
