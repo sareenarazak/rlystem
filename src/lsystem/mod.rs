c767d48a1758b62e80c3261d5fde8278a5f2316f
sareenarazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-07 19:47:27 -0500

    Refactor turtle and L System symbol

--- src/lsystem/mod.rs
+++ src/lsystem/mod.rs
@@ -3,15 +3,34 @@ mod test;
 
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::path::PathBuf;
+use image::ImageError;
+use crate::turtle;
+use crate::turtle::Turtle;
+
+trait LSystemSymbol {
+    fn run_turtle(&self, turtle: &mut Turtle);
+}
+
+impl LSystemSymbol for char {
+    fn run_turtle(&self, turtle: &mut Turtle) {
+        match self {
+            'F' => turtle.forward(4),
+            '+' => turtle.turn(90.0),
+            '-' => turtle.turn(-90.0),
+            _ => panic!("Invalid character")
+        }
+    }
+}
 
 pub struct LSystem<T>
-where T: PartialEq + Eq + Hash + Clone {
+where T: PartialEq + Eq + Hash + Clone + LSystemSymbol {
     pub state: Vec<T>,
     rules: HashMap<T, Vec<T>>,
 }
 
 impl<T> LSystem<T>
-where T: PartialEq + Eq + Hash + Clone {
+where T: PartialEq + Eq + Hash + Clone + LSystemSymbol {
     pub fn new(state: Vec<T>, rules: HashMap<T, Vec<T>>) -> LSystem<T> {
         LSystem{state, rules}
     }
@@ -32,6 +51,15 @@ where T: PartialEq + Eq + Hash + Clone {
         for _ in 1..=n {
             self.update();
         }
+    }
+
+    pub fn draw(&self, path: PathBuf) -> Result<(), ImageError> {
+        let mut turtle = turtle::Turtle::new(1000, 1000);
+        for x in &self.state {
+            x.run_turtle(&mut turtle);
+        }
+
+        turtle.save(path)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,9 @@ fn main() {
     rules.insert('F', "F+F-F-F+F".chars().collect());
     let mut koch_curve = lsystem::LSystem::new(vec!['F'], rules.clone());
 
-    koch_curve.run(3);
+    koch_curve.run(10);
+    koch_curve.draw("img.png".into()).unwrap();
 
-    let mut turtle = turtle::Turtle::new(100, 100);
-    for x in koch_curve.state {
-        match x {
-            'F' => turtle.forward(3),
-            '+' => turtle.turn(90.0),
-            '-' => turtle.turn(-90.0),
-            _ => panic!("Invalid character")
-        }
-    }
 
-    turtle.save("img.png".into()).unwrap()
 }
 
